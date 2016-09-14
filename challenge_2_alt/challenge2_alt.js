729863f43636365af36a1e0c374712271c40ed6d
Marlow14 <c_a_marlow14@yahoo.com>
2016-09-14 12:51:19 -0400

    game first commit

--- challenge_2_alt/challenge2_alt.js
+++ challenge_2_alt/challenge2_alt.js
@@ -30,6 +30,59 @@
 
  $(function(){
 
+ 	var resCount = $("#num-resets").text();
 
+ 	var tmOneShotNum = $("#teamone-numshots").text();
+ 	var tmTwoShotNum = $("#teamtwo-numshots").text();
 
+ 	var tmOneGoal = $("#teamone-numhits").text();
+ 	var tmTwoGoal = $("#teamtwo-numhits").text();
+
+ 	$(".right").data("two", { first: 0, last: 0 } );
+ 	$(".left").data("one", { first: 0, last: 0 } );
+
+//Tests value to console
+ 	console.log(resCount);
+
+//Reset 
+ 	$("#reset").click(function(){
+ 		resCount++
+ 		$("#num-resets").text(resCount);
+ 		$( "#teamtwo-numshots:first" ).text( $( ".right" ).data( "two" ).first );
+		$( "#teamtwo-numhits:last" ).text( $( ".right" ).data( "two" ).last );
+		$( "#teamone-numshots:first" ).text( $( ".left" ).data( "one" ).first );
+		$( "#teamone-numhits:last" ).text( $( ".left" ).data( "one" ).last );
+ 	});
+
+//Team one shot attempts 		
+ 	$( "#teamone-shoot" ).click(function(){
+ 	
+ 		var teamOneAttempt = Math.floor(Math.random()*6);
+ 			
+		if (teamOneAttempt % 2 == 0){
+ 			tmOneGoal++
+ 			tmOneShotNum++
+ 			$("#teamone-numhits").text(tmOneGoal);
+			$( "body" ).css( "background-color", "red" );
+ 		}else{
+			tmOneShotNum++
+			$("body").css("background-color", "white");
+ 		}
+ 		$("#teamone-numshots").text(tmOneShotNum);
+ 	});
+
+	$( "#teamtwo-shoot" ).click(function(){
+		var teamTwoAttempt = Math.floor(Math.random()*6);
+
+		if (teamTwoAttempt % 2 == 0){
+			tmTwoGoal++
+			tmTwoShotNum++
+			$("#teamtwo-numhits").text(tmTwoGoal);
+	  		$( "body" ).css( "background-color", "blue" );
+  		}else{
+  			tmTwoShotNum++
+			$("body").css("background-color", "white");
+  		}
+  		$("#teamtwo-numshots").text(tmTwoShotNum);
+	});
  })
