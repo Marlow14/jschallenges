f96abba7d56e928ccd3c35e23cf9c5410119aa07
Marlow14 <c_a_marlow14@yahoo.com>
2016-09-13 22:35:04 -0400

    Complete all except change bacon words

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,30 @@
 $(function(){
 
-	//all your code should go here
+//all your code should go here
+
+// (1) Make Feed Template toggle color on click
+	var colorSet = true;
+
+	$( "h1" ).on( "click", function() {
+		if(colorSet)
+			$(this).css('color', 'red');
+  	else{
+  		$(this).css('color', 'black');
+  		}
+  		colorSet = !colorSet;
+  	});
+
+// (2) Hide every odd list element
+  	$("h5:odd").hide();
+
+// (3)
+	$("p").text($("p").text().replace("bacon", "Laser Vison"));
+
+// (4)
+	$(".row.post:gt(3)").remove();
+
+// (5) Removed the two images from right side of page
+  	$("p img").remove();
 
 })
+
